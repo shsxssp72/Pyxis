53867e87bc3a219ca4e0ea7748c016d3c9ca006a
shsxssp72 <shsxssp72@hotmail.com>
2021-01-21 21:26:00 +0800

    Add clipboard sink

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import logging
 import time
 from typing import AnyStr, Dict
 
-from components.commit_handler import GoogleTranslationCommitHandler, translation_providers
+from components.commit_handler import translation_providers
 from components.data_storage import DefaultZhMemoryDataStorage, AbstractDataStorage, DefaultEnMemoryDataStorage
 from components.event_handler import DefaultEventHandler, IEventHandler
 from components.event_listener import KeyboardEventListener, IEventListener
-from components.sink import ScreenSink, FileSink
+from components.sink import ScreenSink, FileSink, ClipboardSink
 from utils.logging_base import LoggingBase
 
 
@@ -22,6 +22,8 @@ class Core(LoggingBase):
             self.data_storage: AbstractDataStorage = DefaultEnMemoryDataStorage()
         if not config['no-result-to-screen']:
             self.data_storage.register_sink('screen_sink', ScreenSink())
+        if config['result-to-clipboard']:
+            self.data_storage.register_sink('clipboard_sink', ClipboardSink())
         if config['output-file'] is not None:
             self.data_storage.register_sink('file_sink', FileSink(config['output_file']))
 
@@ -55,6 +57,7 @@ def load_config(config_file_path: AnyStr):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--no-result-to-stdout', dest='no_result_to_screen', action='store_true')
+    parser.add_argument('--result-to-clipboard', dest='result_to_clipboard', action='store_true')
     parser.add_argument('-L', '--log-level', dest='log_level', type=str, default="WARNING")
     parser.add_argument('-s', '--source-language', dest='source_language', type=str, default="en")
     parser.add_argument('-t', '--target-language', dest='target_language', type=str, default="zh")
@@ -65,6 +68,7 @@ if __name__ == '__main__':
 
     app_config: Dict = load_config(config_file_path=args.config_file)
     app_config['no-result-to-screen'] = args.no_result_to_screen
+    app_config['result-to-clipboard'] = args.result_to_clipboard
     app_config['log-level'] = args.log_level
     app_config['source-language'] = args.source_language
     app_config['target-language'] = args.target_language
