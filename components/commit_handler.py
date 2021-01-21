d4b9d97fcd0c085c3eab6d14a69f8cc483549e49
shsxssp72 <shsxssp72@hotmail.com>
2021-01-21 10:43:21 +0800

    Add language and translation provider option

--- components/commit_handler.py
+++ components/commit_handler.py
@@ -41,13 +41,17 @@ class TransparentCommitHandler(AbstractCommitHandler):
 
 
 class AsyncTranslationCommitHandler(AbstractCommitHandler):
+    def __init__(self, target_language: AnyStr):
+        super().__init__()
+        self.target_language: AnyStr = target_language
+
     def commit(self, data: AnyStr) -> None:
         self.logger.debug('Async invoke translator.')
         thread = threading.Thread(target=lambda: self.work(data))
         thread.start()
 
     def work(self, data: AnyStr):
-        result = translators.google(query_text=data, to_language='zh-CN')
+        result = self.translate(data=data)
         for callback_name, callback in self.callbacks.items():
             self.logger.debug(f'Callback {callback_name} invoked.')
             callback(result)
@@ -59,19 +63,43 @@ class AsyncTranslationCommitHandler(AbstractCommitHandler):
 
 class GoogleTranslationCommitHandler(AsyncTranslationCommitHandler):
     def translate(self, data: AnyStr) -> AnyStr:
-        return translators.google(query_text=data, to_language='zh-CN')
+        if self.target_language == 'zh':
+            to_language: AnyStr = 'zh-CN'
+        else:
+            to_language: AnyStr = 'en'
+        return translators.google(query_text=data, to_language=to_language)
 
 
 class BingTranslationCommitHandler(AsyncTranslationCommitHandler):
     def translate(self, data: AnyStr) -> AnyStr:
-        return translators.bing(query_text=data, to_language='zh-Hans')
+        if self.target_language == 'zh':
+            to_language: AnyStr = 'zh-Hans'
+        else:
+            to_language: AnyStr = 'en'
+        return translators.bing(query_text=data, to_language=to_language)
 
 
 class BaiduTranslationCommitHandler(AsyncTranslationCommitHandler):
     def translate(self, data: AnyStr) -> AnyStr:
-        return translators.baidu(query_text=data, to_language='zh')
+        if self.target_language == 'zh':
+            to_language: AnyStr = 'zh'
+        else:
+            to_language: AnyStr = 'en'
+        return translators.baidu(query_text=data, to_language=to_language)
 
 
 class TencentTranslationCommitHandler(AsyncTranslationCommitHandler):
     def translate(self, data: AnyStr) -> AnyStr:
-        return translators.tencent(query_text=data, to_language='zh')
+        if self.target_language == 'zh':
+            to_language: AnyStr = 'zh'
+        else:
+            to_language: AnyStr = 'en'
+        return translators.tencent(query_text=data, to_language=to_language)
+
+
+translation_providers: Dict[AnyStr, ICommitHandler.__class__] = {
+    'google': GoogleTranslationCommitHandler,
+    'bing': BingTranslationCommitHandler,
+    'baidu': BaiduTranslationCommitHandler,
+    'tencent': TencentTranslationCommitHandler,
+}
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import logging
 import time
 from typing import AnyStr, Dict
 
-from components.commit_handler import GoogleTranslationCommitHandler
+from components.commit_handler import GoogleTranslationCommitHandler, translation_providers
 from components.data_storage import DefaultZhMemoryDataStorage, AbstractDataStorage, DefaultEnMemoryDataStorage
 from components.event_handler import DefaultEventHandler, IEventHandler
 from components.event_listener import KeyboardEventListener, IEventListener
@@ -16,7 +16,7 @@ class Core(LoggingBase):
     def __init__(self, config: Dict):
         super().__init__()
         self.config: Dict = config
-        if config['language'] == 'zh':
+        if config['source-language'] == 'zh':
             self.data_storage: AbstractDataStorage = DefaultZhMemoryDataStorage()
         else:
             self.data_storage: AbstractDataStorage = DefaultEnMemoryDataStorage()
@@ -24,7 +24,11 @@ class Core(LoggingBase):
             self.data_storage.register_sink('screen_sink', ScreenSink())
         if config['output-file'] is not None:
             self.data_storage.register_sink('file_sink', FileSink(config['output_file']))
-        self.data_storage.register_commit_handler('google', GoogleTranslationCommitHandler())
+
+        self.data_storage.register_commit_handler(commit_handler_name=config['translation-provider'],
+                                                  commit_handler=translation_providers.get(
+                                                      config['translation-provider'].lower())(
+                                                      target_language=config['target-language']))
         self.event_handler: IEventHandler = DefaultEventHandler(data_storage=self.data_storage)
         self.event_listener: IEventListener = KeyboardEventListener(key_bindings=self.config['key-bindings'],
                                                                     event_handler=self.event_handler)
@@ -52,7 +56,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--no-result-to-stdout', dest='no_result_to_screen', action='store_true')
     parser.add_argument('-L', '--log-level', dest='log_level', type=str, default="WARNING")
-    parser.add_argument('-l', '--language', dest='language', type=str, default="en")
+    parser.add_argument('-s', '--source-language', dest='source_language', type=str, default="en")
+    parser.add_argument('-t', '--target-language', dest='target_language', type=str, default="zh")
+    parser.add_argument('-p', '--translation-provider', dest='translation_provider', type=str, default="google")
     parser.add_argument('-o', '--output', dest='output_file', type=str)
     parser.add_argument('-c', '--config', dest='config_file', type=str, default="config.json")
     args = parser.parse_args()
@@ -60,7 +66,9 @@ if __name__ == '__main__':
     app_config: Dict = load_config(config_file_path=args.config_file)
     app_config['no-result-to-screen'] = args.no_result_to_screen
     app_config['log-level'] = args.log_level
-    app_config['language'] = args.language
+    app_config['source-language'] = args.source_language
+    app_config['target-language'] = args.target_language
+    app_config['translation-provider'] = args.translation_provider
     app_config['output-file'] = args.output_file
 
     logging.basicConfig(level=app_config['log-level'],
