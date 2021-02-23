b831566fd9ab8a880f08b65bdfd4ba81002684b7
shsxssp72 <shsxssp72@hotmail.com>
2021-02-23 23:55:40 +0800

    Add deepl translator

--- components/commit_handler.py
+++ components/commit_handler.py
@@ -1,8 +1,13 @@
 from abc import ABC, abstractmethod
-from typing import AnyStr, Callable, Dict
+from typing import AnyStr, Callable, Dict, Set
 import translators
 import threading
 from utils.logging_base import LoggingBase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class ICommitHandler(ABC):
@@ -101,10 +106,91 @@ class TencentTranslationCommitHandler(AsyncTranslationCommitHandler):
         return translators.tencent(query_text=data, to_language=to_language)
 
 
+class HeadlessBrowserConfig(object):
+    page_load_wait_secs: int = 5
+    page_load_timeout_secs: int = 10
+
+
+class HeadlessBrowserTranslationCommitHandler(AsyncTranslationCommitHandler, ABC):
+    def __init__(self, target_language: AnyStr):
+        super().__init__(target_language)
+        profile = webdriver.FirefoxProfile()
+        profile.set_preference("browser.privatebrowsing.autostart", True)
+        options = Options()
+        options.add_argument("--headless")
+        self.driver = webdriver.Firefox(firefox_profile=profile, options=options)
+        self.driver.implicitly_wait(HeadlessBrowserConfig.page_load_wait_secs)
+        self.driver.set_page_load_timeout(HeadlessBrowserConfig.page_load_timeout_secs)
+        self.wait = WebDriverWait(self.driver, HeadlessBrowserConfig.page_load_timeout_secs)
+
+
+class DeepLBrowserTranslationCommitHandler(HeadlessBrowserTranslationCommitHandler):
+    available_language: Set[AnyStr] = {
+        'auto', 'pl', 'de', 'ru',
+        'fr', 'nl', 'pt', 'ja',
+        'es', 'it', 'en', 'zh'
+    }
+
+    class TextNotContainedInElement(object):
+        def __init__(self, locator, match_set: Set[AnyStr]):
+            self.locator = locator
+            self.match_set: Set[AnyStr] = match_set
+
+        def __call__(self, driver):
+            try:
+                element_text: AnyStr = driver.find_element(*self.locator).get_attribute('value')
+                if element_text is not None and element_text.strip() != '':
+                    for match_item in self.match_set:
+                        if match_item in element_text:
+                            return False
+                        else:
+                            continue
+                    return element_text
+                else:
+                    return False
+            except:
+                return False
+
+    def __init__(self, target_language: AnyStr):
+        super().__init__(target_language)
+        self.driver.get("https://www.deepl.com/translator")
+        self.targetLanguageBtn = self.driver.find_element_by_xpath(
+            "//button[contains(@dl-test,'translator-target-lang-btn')]")
+        # inputLanguageBtn = driver.find_element_by_xpath("//button[contains(@dl-test,'translator-source-lang-btn')]")
+        # inputLanguageBtn.click()
+        # wait.until(expected_conditions.presence_of_element_located(
+        #     (By.XPATH, '//div[contains(@dl-test,"translator-source-lang-list")]')))
+        # driver.find_element_by_xpath(
+        #     "//button[contains(@dl-test,'translator-lang-option-" + 'en' + "')]").click()
+        self.targetLanguageBtn.click()
+        self.wait.until(expected_conditions.presence_of_element_located(
+            (By.XPATH, '//div[contains(@dl-test,"translator-target-lang-list")]')))
+        self.driver.find_element_by_xpath(
+            f'//button[contains(@dl-test,"translator-lang-option-{self.target_language}")]').click()
+        self.original_input = self.driver.find_element_by_xpath(
+            '//textarea[contains(@dl-test,"translator-source-input")]')
+        # Prevent translating status
+        self.translating_status_indicators: Set[AnyStr] = {'[...]'}
+        self.logger.info('DeepL Ready')
+
+    def translate(self, data: AnyStr) -> AnyStr:
+        self.original_input.clear()
+        self.original_input.send_keys(data)
+        result = self.wait.until(self.TextNotContainedInElement(
+            (By.XPATH, '//textarea[contains(@dl-test,"translator-target-input")]'),
+            self.translating_status_indicators))
+        self.original_input.clear()
+        return result
+
+    def __del__(self):
+        self.driver.quit()
+
+
 translation_providers: Dict[AnyStr, ICommitHandler.__class__] = {
     'google': GoogleTranslationCommitHandler,
     'bing': BingTranslationCommitHandler,
     'baidu': BaiduTranslationCommitHandler,
     'tencent': TencentTranslationCommitHandler,
     'transparent': TransparentCommitHandler,
+    'deepl': DeepLBrowserTranslationCommitHandler,
 }
--- main.py
+++ main.py
@@ -58,10 +58,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--no-result-to-stdout', dest='no_result_to_screen', action='store_true')
     parser.add_argument('--result-to-clipboard', dest='result_to_clipboard', action='store_true')
-    parser.add_argument('-L', '--log-level', dest='log_level', type=str, default="WARNING")
-    parser.add_argument('-s', '--source-language', dest='source_language', type=str, default="en")
-    parser.add_argument('-t', '--target-language', dest='target_language', type=str, default="zh")
-    parser.add_argument('-p', '--translation-provider', dest='translation_provider', type=str, default="google")
+    parser.add_argument('-L', '--log-level', dest='log_level', type=str)
+    parser.add_argument('-s', '--source-language', dest='source_language', type=str)
+    parser.add_argument('-t', '--target-language', dest='target_language', type=str)
+    parser.add_argument('-p', '--translation-provider', dest='translation_provider', type=str)
     parser.add_argument('-o', '--output', dest='output_file', type=str)
     parser.add_argument('-c', '--config', dest='config_file', type=str, default="config.json")
     args = parser.parse_args()
@@ -69,10 +69,14 @@ if __name__ == '__main__':
     app_config: Dict = load_config(config_file_path=args.config_file)
     app_config['no-result-to-screen'] = args.no_result_to_screen
     app_config['result-to-clipboard'] = args.result_to_clipboard
-    app_config['log-level'] = args.log_level
-    app_config['source-language'] = args.source_language
-    app_config['target-language'] = args.target_language
-    app_config['translation-provider'] = args.translation_provider
+    if args.log_level is not None:
+        app_config['log-level'] = args.log_level
+    if args.source_language is not None:
+        app_config['source-language'] = args.source_language
+    if args.target_language is not None:
+        app_config['target-language'] = args.target_language
+    if args.translation_provider is not None:
+        app_config['translation-provider'] = args.translation_provider
     app_config['output-file'] = args.output_file
 
     logging.basicConfig(level=app_config['log-level'],
